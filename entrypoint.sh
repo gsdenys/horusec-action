c41852bb168a7b04f61571cfaf8b538c8a667448
Denys G. Santos <gsdenys@gmail.com>
2022-04-12 21:47:40 -0300

    changeback to gsdenys

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-
-mkdir /opt/data
-cp -r /github/workspace/* /opt/data
-
-chmod -r 777 /opt/data
-
-
 horusec version
 
-horusec start -e="true" $* -p /opt/data -P /opt/data
+horusec start -e="true" $*
